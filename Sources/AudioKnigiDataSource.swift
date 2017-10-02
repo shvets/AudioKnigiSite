d381545bf442ae2a92fd5626da9390ac8ec8bf68
Alexander Shvets <alexander.shvets@gmail.com>
2017-10-02 11:03:54 -0400

    remove dependency on SwiftyJSON

--- Sources/AudioKnigiDataSource.swift
+++ Sources/AudioKnigiDataSource.swift
@@ -1,12 +1,14 @@
 import SwiftyJSON
 import WebAPI
 import TVSetKit
+import AudioPlayer
 
 class AudioKnigiDataSource: DataSource {
   let service = AudioKnigiService.shared
 
   override open func load(params: Parameters) throws -> [Any] {
-    var result: [Any] = []
+    var result: Any?
+    var tracks = false
 
     let selectedItem = params["selectedItem"] as? MediaItem
 
@@ -19,175 +21,185 @@ class AudioKnigiDataSource: DataSource {
     }
 
     switch request {
-      case "Bookmarks":
-        if let bookmarks = params["bookmarks"]  as? Bookmarks {
-          bookmarks.load()
-          result = bookmarks.getBookmarks(pageSize: 60, page: currentPage)
-        }
+    case "Bookmarks":
+      if let bookmarks = params["bookmarks"]  as? Bookmarks {
+        bookmarks.load()
+        result = bookmarks.getBookmarks(pageSize: 60, page: currentPage)
+      }
 
-      case "History":
-        if let history = params["history"] as? History {
-          history.load()
-          result = history.getHistoryItems(pageSize: 60, page: currentPage)
-        }
+    case "History":
+      if let history = params["history"] as? History {
+        history.load()
+        result = history.getHistoryItems(pageSize: 60, page: currentPage)
+      }
 
-      case "Genre Books":
-        let path = selectedItem!.id
+    case "Genre Books":
+      let path = selectedItem!.id
 
-        result = try service.getBooks(path: path!, page: currentPage)["movies"] as! [Any]
+      result = try service.getBooks(path: path!, page: currentPage)["movies"] as! [Any]
 
-      case "New Books":
-        result = try service.getNewBooks(page: currentPage)["movies"] as! [Any]
+    case "New Books":
+      result = try service.getNewBooks(page: currentPage)["movies"] as! [Any]
 
-      case "Best Books":
-        var period = "all"
+    case "Best Books":
+      var period = "all"
 
-        if selectedItem!.name == "By Month" {
-          period = "30"
-        }
-        else if selectedItem!.name == "By Week" {
-          period = "7"
-        }
+      if selectedItem!.name == "By Month" {
+        period = "30"
+      }
+      else if selectedItem!.name == "By Week" {
+        period = "7"
+      }
 
-        result = try service.getBestBooks(period: period, page: currentPage)["movies"] as! [Any]
+      result = try service.getBestBooks(period: period, page: currentPage)["movies"] as! [Any]
 
-      case "All Authors":
-        result = try service.getAuthors(page: currentPage)["movies"] as! [Any]
+    case "All Authors":
+      result = try service.getAuthors(page: currentPage)["movies"] as! [Any]
 
-      case "Authors Letters":
-        let letters = getLetters(AudioKnigiService.Authors)
+    case "Authors Letters":
+      let letters = getLetters(AudioKnigiService.Authors)
 
-        var list = [Any]()
+      var list = [Any]()
 
-        list.append(["name": "Все"])
+      list.append(["name": "Все"])
 
-        for letter in letters {
-          list.append(["name": letter])
-        }
+      for letter in letters {
+        list.append(["name": letter])
+      }
 
-        result = list
+      result = list
 
-      case "Authors Letter Groups":
-        if let letter = params["parentId"] as? String {
-          var letterGroups = [Any]()
+    case "Authors Letter Groups":
+      if let letter = params["parentId"] as? String {
+        var letterGroups = [Any]()
 
-          for author in AudioKnigiService.Authors {
-            let groupName = author.key
-            let group = author.value
+        for author in AudioKnigiService.Authors {
+          let groupName = author.key
+          let group = author.value
 
-            if groupName[groupName.startIndex] == letter[groupName.startIndex] {
-              var newGroup: [Any] = []
+          if groupName[groupName.startIndex] == letter[groupName.startIndex] {
+            var newGroup: [Any] = []
 
-              for el in group {
-                newGroup.append(["id": el.id, "name": el.name])
-              }
-
-              letterGroups.append(["name": groupName, "items": newGroup])
+            for el in group {
+              newGroup.append(["id": el.id, "name": el.name])
             }
-          }
 
-          result = letterGroups
+            letterGroups.append(["name": groupName, "items": newGroup])
+          }
         }
 
-      case "Authors":
-        result = (selectedItem as! AudioKnigiMediaItem).items
+        result = letterGroups
+      }
 
-      case "Author":
-        let path = selectedItem!.id
+    case "Authors":
+      result = (selectedItem as! AudioKnigiMediaItem).items
 
-        result = try service.getBooks(path: path!, page: currentPage)["movies"] as! [Any]
+    case "Author":
+      let path = selectedItem!.id
+
+      result = try service.getBooks(path: path!, page: currentPage)["movies"] as! [Any]
 
     case "Performers Letters":
-        let letters = getLetters(AudioKnigiService.Performers)
+      let letters = getLetters(AudioKnigiService.Performers)
 
-        var list = [Any]()
+      var list = [Any]()
 
-        list.append(["name": "Все"])
+      list.append(["name": "Все"])
 
-        for letter in letters {
-          list.append(["name": letter])
-        }
+      for letter in letters {
+        list.append(["name": letter])
+      }
 
-        result = list
+      result = list
 
-      case "Performers Letter Groups":
-        if let letter = params["parentId"] as? String {
-          var letterGroups = [Any]()
+    case "Performers Letter Groups":
+      if let letter = params["parentId"] as? String {
+        var letterGroups = [Any]()
 
-          for performer in AudioKnigiService.Performers {
-            let groupName = performer.key
-            let group = performer.value
+        for performer in AudioKnigiService.Performers {
+          let groupName = performer.key
+          let group = performer.value
 
-            if groupName[groupName.startIndex] == letter[groupName.startIndex] {
-              var newGroup: [Any] = []
+          if groupName[groupName.startIndex] == letter[groupName.startIndex] {
+            var newGroup: [Any] = []
 
-              for el in group {
-                newGroup.append(["id": el.id, "name": el.name])
-              }
-
-              letterGroups.append(["name": groupName, "items": newGroup])
+            for el in group {
+              newGroup.append(["id": el.id, "name": el.name])
             }
-          }
 
-          result = letterGroups
-        }
-
-      case "Performers":
-        result = (selectedItem as! AudioKnigiMediaItem).items
-
-      case "Performer":
-        let path = selectedItem!.id
-
-        result = try service.getBooks(path: path!, page: currentPage)["movies"] as! [Any]
-
-      case "All Performers":
-        result = try service.getPerformers(page: currentPage)["movies"] as! [Any]
-
-      case "Genres":
-        result = try service.getGenres(page: currentPage)["movies"] as! [Any]
-
-      case "Tracks":
-        let url = selectedItem!.id!
-
-        result = try service.getAudioTracks(url)
-
-      case "Search":
-        if let query = params["query"] as? String {
-          if !query.isEmpty {
-            result = try service.search(query, page: currentPage)["movies"] as! [Any]
-          }
-          else {
-            result = []
+            letterGroups.append(["name": groupName, "items": newGroup])
           }
         }
 
-      default:
-        result = []
+        result = letterGroups
+      }
+
+    case "Performers":
+      result = (selectedItem as! AudioKnigiMediaItem).items
+
+    case "Performer":
+      let path = selectedItem!.id
+
+      result = try service.getBooks(path: path!, page: currentPage)["movies"] as! [Any]
+
+    case "All Performers":
+      result = try service.getPerformers(page: currentPage)["movies"] as! [Any]
+
+    case "Genres":
+      result = try service.getGenres(page: currentPage)["movies"] as! [Any]
+
+    case "Tracks":
+      let url = selectedItem!.id!
+
+      tracks = true
+      result = try service.getAudioTracks(url)
+
+    case "Search":
+      if let query = params["query"] as? String {
+        if !query.isEmpty {
+          result = try service.search(query, page: currentPage)["movies"] as! [Any]
+        }
+        else {
+          result = []
+        }
+      }
+
+    default:
+      result = nil
     }
 
     let convert = params["convert"] as? Bool ?? true
 
-    if convert {
+    if convert || tracks {
       return convertToMediaItems(result)
     }
     else {
-      return result
+      return result as! [Any]
     }
   }
 
-  func convertToMediaItems(_ items: [Any]) -> [MediaItem] {
-    var newItems = [MediaItem]()
+  func convertToMediaItems(_ items: Any) -> [Any] {
+    var newItems = [Any]()
 
-    for item in items {
-      var jsonItem = item as? JSON
+    if let tracks = items as? [Track] {
+      for track in tracks {
+        var item = AudioItem(name: track.title + ".mp3", id: track.url)
 
-      if jsonItem == nil {
-        jsonItem = JSON(item)
+        newItems += [item]
       }
+    }
+    else if let items = items as? [Any] {
+      for item in items {
+        var jsonItem = item as? JSON
 
-      let movie = AudioKnigiMediaItem(data: jsonItem!)
+        if jsonItem == nil {
+          jsonItem = JSON(item)
+        }
 
-      newItems += [movie]
+        let movie = AudioKnigiMediaItem(data: jsonItem!)
+
+        newItems += [movie]
+      }
     }
 
     return newItems
