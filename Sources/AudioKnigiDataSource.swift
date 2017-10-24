5f0928c5ed58573db98186ac6618a8e17dd4b213
Alexander Shvets <alexander.shvets@gmail.com>
2017-10-24 13:41:26 -0400

    code improvement

--- Sources/AudioKnigiDataSource.swift
+++ Sources/AudioKnigiDataSource.swift
@@ -6,8 +6,7 @@ class AudioKnigiDataSource: DataSource {
   let service = AudioKnigiService.shared
 
   override open func load(params: Parameters) throws -> [Any] {
-    var result: Any?
-    var tracks = false
+    var items = [Item]()
 
     let selectedItem = params["selectedItem"] as? Item
 
@@ -20,24 +19,32 @@ class AudioKnigiDataSource: DataSource {
 
     switch request {
     case "Bookmarks":
-      if let bookmarksManager = params["bookmarksManager"]  as? BookmarksManager,
+      if let bookmarksManager = params["bookmarksManager"] as? BookmarksManager,
          let bookmarks = bookmarksManager.bookmarks {
-        result = bookmarks.getBookmarks(pageSize: 60, page: currentPage)
+        let data = bookmarks.getBookmarks(pageSize: 60, page: currentPage)
+
+        items = adjustItems(data)
       }
 
     case "History":
-      if let historyManager = params["historyManager"]  as? HistoryManager,
+      if let historyManager = params["historyManager"] as? HistoryManager,
          let history = historyManager.history {
-        result = history.getHistoryItems(pageSize: 60, page: currentPage)
+        let data = history.getHistoryItems(pageSize: 60, page: currentPage)
+
+        items = adjustItems(data)
       }
 
     case "Genre Books":
-      if let selectedItem = selectedItem, let path = selectedItem.id {
-        result = try service.getBooks(path: path, page: currentPage)["movies"] as! [Any]
+      if let selectedItem = selectedItem,
+         let path = selectedItem.id,
+         let data = try service.getBooks(path: path, page: currentPage)["movies"] as? [Any] {
+        items = adjustItems(data)
       }
 
     case "New Books":
-      result = try service.getNewBooks(page: currentPage)["movies"] as! [Any]
+      if let data = try service.getNewBooks(page: currentPage)["movies"] as? [Any] {
+        items = adjustItems(data)
+      }
 
     case "Best Books":
       var period = "all"
@@ -49,226 +56,219 @@ class AudioKnigiDataSource: DataSource {
         else if selectedItem.name == "By Week" {
           period = "7"
         }
-        
-        result = try service.getBestBooks(period: period, page: currentPage)["movies"] as! [Any]
+
+        if let data = try service.getBestBooks(period: period, page: currentPage)["movies"] as? [Any] {
+          items = adjustItems(data)
+        }
       }
 
     case "All Authors":
-      result = try service.getAuthors(page: currentPage)["movies"] as! [Any]
-
-    case "Authors Letters":
-      let letters = getLetters(AudioKnigiService.Authors)
-
-      var list = [Any]()
-
-      list.append(["name": "Все"])
-
-      for letter in letters {
-        list.append(["name": letter])
+      if let data = try service.getAuthors(page: currentPage)["movies"] as? [Any] {
+        items = adjustItems(data)
       }
 
-      result = list
+    case "Authors Letters":
+      items = adjustItems(getLettersItems(AudioKnigiService.Authors))
 
     case "Authors Letter Groups":
       if let letter = params["parentId"] as? String {
-        var letterGroups = [Any]()
-
-        for author in AudioKnigiService.Authors {
-          let groupName = author.key
-          let group = author.value
-
-          if groupName[groupName.startIndex] == letter[groupName.startIndex] {
-            var newGroup: [Any] = []
-
-            for el in group {
-              newGroup.append(["id": el.id, "name": el.name])
-            }
-
-            letterGroups.append(["name": groupName, "items": newGroup])
-          }
-        }
-
-        result = letterGroups
+        items = adjustItems(getLetterGroups(AudioKnigiService.Authors, letter: letter))
       }
 
     case "Authors":
       if let selectedItem = selectedItem as? AudioKnigiMediaItem {
-        result = selectedItem.items
+        items = adjustItems(selectedItem.items)
       }
 
     case "Author":
-      if let selectedItem = selectedItem {
-        let path = selectedItem.id
-        
-        result = try service.getBooks(path: path!, page: currentPage)["movies"] as! [Any]
+      if let selectedItem = selectedItem,
+         let path = selectedItem.id,
+         let data = try service.getBooks(path: path, page: currentPage)["movies"] as? [Any] {
+        items = adjustItems(data)
       }
 
     case "Performers Letters":
-      let letters = getLetters(AudioKnigiService.Performers)
-
-      var list = [Any]()
-
-      list.append(["name": "Все"])
-
-      for letter in letters {
-        list.append(["name": letter])
-      }
-
-      result = list
+      items = adjustItems(getLettersItems(AudioKnigiService.Performers))
 
     case "Performers Letter Groups":
       if let letter = params["parentId"] as? String {
-        var letterGroups = [Any]()
-
-        for performer in AudioKnigiService.Performers {
-          let groupName = performer.key
-          let group = performer.value
-
-          if groupName[groupName.startIndex] == letter[groupName.startIndex] {
-            var newGroup: [Any] = []
-
-            for el in group {
-              newGroup.append(["id": el.id, "name": el.name])
-            }
-
-            letterGroups.append(["name": groupName, "items": newGroup])
-          }
-        }
-
-        result = letterGroups
+        items = adjustItems(getLetterGroups(AudioKnigiService.Performers, letter: letter))
       }
 
     case "Performers":
       if let selectedItem = selectedItem as? AudioKnigiMediaItem {
-        result = selectedItem.items
+        items = adjustItems(selectedItem.items)
       }
 
     case "Performer":
-      if let selectedItem = selectedItem {
-        let path = selectedItem.id
-        
-        result = try service.getBooks(path: path!, page: currentPage)["movies"] as! [Any]
+      if let selectedItem = selectedItem,
+         let path = selectedItem.id,
+         let data = try service.getBooks(path: path, page: currentPage)["movies"] as? [Any] {
+
+        items = adjustItems(data)
       }
 
     case "All Performers":
-      result = try service.getPerformers(page: currentPage)["movies"] as! [Any]
+      if let data = try service.getPerformers(page: currentPage)["movies"] as? [Any] {
+        items = adjustItems(data)
+      }
 
     case "Genres":
-      result = try service.getGenres(page: currentPage)["movies"] as! [Any]
+      if let data = try service.getGenres(page: currentPage)["movies"] as? [Any] {
+        items = adjustItems(data)
+      }
 
     case "Tracks":
-      let url = selectedItem!.id!
-
-      tracks = true
-      result = try service.getAudioTracks(url)
+      if let selectedItem = selectedItem,
+         let url = selectedItem.id {
+         let tracks = try service.getAudioTracks(url)
+        items = adjustItems(tracks)
+      }
 
     case "Search":
       if let query = params["query"] as? String {
-        if !query.isEmpty {
-          result = try service.search(query, page: currentPage)["movies"] as! [Any]
-        }
-        else {
-          result = []
+        if !query.isEmpty,
+           let data = try service.search(query, page: currentPage)["movies"] as? [Any] {
+          items = adjustItems(data)
         }
       }
 
     default:
-      result = []
+      items = []
     }
 
-    let convert = params["convert"] as? Bool ?? true
-
-    if convert || tracks {
-      return convertToMediaItems(result as Any)
-    }
-    else {
-      return result as! [Any]
-    }
+    return items
   }
 
-  func convertToMediaItems(_ items: Any) -> [Any] {
-    var newItems = [Any]()
+  func adjustItems(_ items: [Any]) -> [Item] {
+    var newItems = [Item]()
 
-    if let tracks = items as? [AudioKnigiAPI.Track] {
-      for track in tracks {
-        let item = AudioItem(name: track.title + ".mp3", id: track.url)
-
-        newItems += [item]
-      }
-    }
-    else if let items = items as? [[String: Any]] {
-      for item in items {
-        let movie = AudioKnigiMediaItem(data: ["name": ""])
-
-        if let dict = item as? [String: String] {
-          movie.name = dict["name"]
-          movie.id = dict["id"]
-          movie.description = dict["description"]
-          movie.thumb = dict["thumb"]
-          movie.type = dict["type"]
-        }
-        else {
-          movie.name = item["name"] as? String
-
-          if let array = item["items"] as? [[String: String]] {
-            var newArray = [AudioKnigiAPI.PersonName]()
-
-            for elem in array {
-              let newElem = AudioKnigiAPI.PersonName(name: elem["name"]!, id: elem["id"]!)
-
-              newArray.append(newElem)
-            }
-
-            movie.items = newArray
-          }
-        }
-
-        newItems += [movie]
-      }
-    }
-    else if let items = items as? [AudioKnigiAPI.PersonName] {
-      for item in items {
-        let movie = AudioKnigiMediaItem(data: ["name": ""])
-        
-        movie.name = item.name
-        movie.id = item.id
-
-        newItems += [movie]
-      }
-    }
-    else if let items = items as? [HistoryItem] {
-      for item in items {
-        let movie = AudioKnigiMediaItem(data: ["name": ""])
-        
-        movie.name = item.item.name
-        movie.id = item.item.id
-        movie.description = item.item.description
-        movie.thumb = item.item.thumb
-        movie.type = item.item.type
-        
-        newItems += [movie]
+    if let items = items as? [HistoryItem] {
+      newItems = transform(items) { item in
+        createHistoryItem(item as! HistoryItem)
       }
     }
     else if let items = items as? [BookmarkItem] {
-      for item in items {
-        let movie = AudioKnigiMediaItem(data: ["name": ""])
-
-        movie.name = item.item.name
-        movie.id = item.item.id
-        movie.description = item.item.description
-        movie.thumb = item.item.thumb
-        movie.type = item.item.type
-
-        newItems += [movie]
+      newItems = transform(items) { item in
+        createBookmarkItem(item as! BookmarkItem)
       }
+    }
+    else if let items = items as? [AudioKnigiAPI.PersonName] {
+      newItems = transform(items) { item in
+        let item = item as! AudioKnigiAPI.PersonName
+
+        return MediaItem(name: item.name, id: String(describing: item.id))
+      }
+    }
+    else if let items = items as? [AudioKnigiAPI.Track] {
+      newItems = transform(items) { item in
+        let track = item as! AudioKnigiAPI.Track
+
+        return MediaItem(name: track.title + ".mp3", id: String(describing: track.url))
+      }
+    }
+
+    else if let items = items as? [[String: Any]] {
+      newItems = transform(items) { item in
+        createMediaItem(item as! [String: Any])
+      }
+    } else if let items = items as? [Item] {
+      newItems = items
     }
 
     return newItems
   }
 
+  func createHistoryItem(_ item: HistoryItem) -> Item {
+    let newItem = MediaItem(data: ["name": ""])
+
+    newItem.name = item.item.name
+    newItem.id = item.item.id
+    newItem.description = item.item.description
+    newItem.thumb = item.item.thumb
+    newItem.type = item.item.type
+
+    return newItem
+  }
+
+  func createBookmarkItem(_ item: BookmarkItem) -> Item {
+    let newItem = MediaItem(data: ["name": ""])
+
+    newItem.name = item.item.name
+    newItem.id = item.item.id
+    newItem.description = item.item.description
+    newItem.thumb = item.item.thumb
+    newItem.type = item.item.type
+
+    return newItem
+  }
+
+  func createMediaItem(_ item: [String: Any]) -> Item {
+    let newItem = AudioKnigiMediaItem(data: ["name": ""])
+
+    if let dict = item as? [String: String] {
+      newItem.name = dict["name"]
+      newItem.id = dict["id"]
+      newItem.description = dict["description"]
+      newItem.thumb = dict["thumb"]
+      newItem.type = dict["type"]
+    } else {
+      newItem.name = item["name"] as? String
+
+      if let array = item["items"] as? [[String: String]] {
+        var newArray = [AudioKnigiAPI.PersonName]()
+
+        for elem in array {
+          let newElem = AudioKnigiAPI.PersonName(name: elem["name"]!, id: elem["id"]!)
+
+          newArray.append(newElem)
+        }
+
+        newItem.items = newArray
+      }
+    }
+
+    return newItem
+  }
+
+    func getLetterGroups(_ list: [NameClassifier.ItemsGroup], letter: String) -> [[String: Any]] {
+    var letterGroups = [[String: Any]]()
+
+    for element in list {
+      let groupName = element.key
+      let group = element.value
+
+      if groupName[groupName.startIndex] == letter[groupName.startIndex] {
+        var newGroup: [Any] = []
+
+        for el in group {
+          newGroup.append(["id": el.id, "name": el.name])
+        }
+
+        letterGroups.append(["name": groupName, "items": newGroup])
+      }
+    }
+    
+    return letterGroups
+  }
+
+  func getLettersItems(_ list: [NameClassifier.ItemsGroup]) -> [[String: Any]] {
+    let letters = getLetters(list)
+
+    var items = [[String: Any]]()
+
+    items.append(["name": "Все"])
+
+    for letter in letters {
+      items.append(["name": letter])
+    }
+
+    return items
+  }
+
   func getLetters(_ items: [NameClassifier.ItemsGroup]) -> [String] {
-    var rletters = [String]()
-    var eletters = [String]()
+    var ruLetters = [String]()
+    var enLetters = [String]()
 
     for item in items {
       let groupName = item.key
@@ -278,18 +278,18 @@ class AudioKnigiDataSource: DataSource {
       let letter = String(groupName[index])
 
       if (letter >= "a" && letter <= "z") || (letter >= "A" && letter <= "Z") {
-        if !eletters.contains(letter) {
-          eletters.append(letter)
+        if !enLetters.contains(letter) {
+          enLetters.append(letter)
         }
       }
       else if (letter >= "а" && letter <= "я") || (letter >= "А" && letter <= "Я") {
-        if !rletters.contains(letter) {
-          rletters.append(letter)
+        if !ruLetters.contains(letter) {
+          ruLetters.append(letter)
         }
       }
     }
 
-    return rletters + eletters
+    return ruLetters + enLetters
   }
 
 }
